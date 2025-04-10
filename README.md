ac75aff029acb236376f7742639bc9e7d58767ea
ntbao <tomaswson@gmail.com>
2025-04-10 12:15:22 +0000

    feat(session01): data type

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # se-typescript-2025
 The project use for training on TypeScript
+
--- session_01_basic/src/lab01_syntax_data_type.ts
+++ session_01_basic/src/lab01_syntax_data_type.ts
@@ -2,7 +2,33 @@
  * * Base type of TypeScript
  */
 
-//? BOOLEAN
+//? boolean
+let isDone : boolean = false
+console.log(`isDone = ${isDone} - ${typeof(isDone)}`)
 
-let isDone: boolean = false;
-console.log(isDone);
+//! try assign number to boolean
+//! isDone = 1; => error TS2322: Type 'number' is not assignable to type 'boolean'.
+
+//? number 
+let decimal : number = 6          // integer
+let hex     : number = 0xf0000d   // hex      => is converted to decimal
+let bin     : number = 0b101010   // binary   => is converted to decimal
+let octal   : number = 0o744      // octal    => is converted to decimal
+
+console.log(`Decimal = ${decimal} - ${typeof(decimal)}`)
+console.log(`Hex     = ${hex} - ${typeof(hex)}`)
+console.log(`Bin     = ${bin} - ${typeof(bin)}`)
+console.log(`Octal   = ${octal} - ${typeof(octal)}`)
+
+let big     : bigint = 100n
+console.log(`Big int = ${big} - ${typeof(big)}`)
+
+//? Double
+let double : number = 0.1 + 0.2
+console.log(`double = ${double} - ${typeof(double)}`)
+
+//? string 
+let color   : string = 'light blue'
+color                = 'dark red'
+console.log(`Color   = ${color} - ${typeof(color)}`)
+
