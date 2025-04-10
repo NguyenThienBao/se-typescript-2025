531db1a60fa2488d8cadd34892b46f2659a0cc08
ntbao <tomaswson@gmail.com>
2025-04-10 14:01:34 +0000

    feat(session01): config run file

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # se-typescript-2025
 The project use for training on TypeScript
 
+Create file bash.sh
+
+```shell
+    filename=$(basename "$1")
+    name="$(filename%.*)"
+    js_file="$name.js"
+
+    tsc && node bin/"$js_file"
+```
+
+Run this in terminal to change build.sh to executable
+
+```shell
+    chmod +x build.sh
+```
+
+Config script in package.json
+
+```json
+    "build": "./build.sh"
+```
+
+Run yarn to compiple and run .js file
+
+```shell
+yarn build ./src/[filename].ts
+```
--- session_01_basic/bin/lab01.syntax.data_type.js
+++ session_01_basic/bin/lab01.syntax.data_type.js
@@ -1,7 +1,6 @@
 "use strict";
 let isDone = false;
 console.log(`isDone = ${isDone} - ${typeof (isDone)}`);
-isDone = 1;
 let decimal = 6;
 let hex = 0xf0000d;
 let bin = 0b101010;
@@ -12,7 +11,9 @@ console.log(`Bin     = ${bin} - ${typeof (bin)}`);
 console.log(`Octal   = ${octal} - ${typeof (octal)}`);
 let big = 100n;
 console.log(`Big int = ${big} - ${typeof (big)}`);
+let double = 0.1 + 0.2;
+console.log(`double = ${double} - ${typeof (double)}`);
 let color = 'light blue';
 color = 'dark red';
 console.log(`Color   = ${color} - ${typeof (color)}`);
-//# sourceMappingURL=lab01_syntax_data_type.js.map
+//# sourceMappingURL=lab01.syntax.data_type.js.map
