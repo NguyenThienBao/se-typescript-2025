3b968c6ba40c61bb7e2509d22164dcd8b43a8204
ntbao <tomaswson@gmail.com>
2025-05-05 12:15:17 +0000

    feat(session02): modules advanced OOP

---
+++ session_02_oop/bin/lab11.modules.namespaces.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lab11_modules_namespaces_1 = require("./lab11.modules.namespaces");
+const fraction = new lab11_modules_namespaces_1.MyLibs.Fraction(1, 2);
+console.log(fraction.toString());
+console.log(`PI = ${lab11_modules_namespaces_1.MyLibs.PI}`);
+console.log(`Triple of 4 = ${lab11_modules_namespaces_1.MyLibs.triple(4)}`);
+//# sourceMappingURL=lab11.modules.namespaces.js.map
