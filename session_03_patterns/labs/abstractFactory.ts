874ea092f956f2a169cfd11139bd2439222ec4f5
ntbao <tomaswson@gmail.com>
2025-06-04 08:30:27 +0000

    feat(session_03): labs folder

---
+++ session_03_patterns/labs/abstractFactory.ts
@@ -0,0 +1,102 @@
+// Factory
+interface AbstractFactory {
+    createProductA(): AbstractProductA;
+    createProductB(): AbstractProductB;
+}
+
+class ConcreteFactory1 implements AbstractFactory {
+    public createProductA(): AbstractProductA {
+        return new ConcreteProductA1();
+    }
+
+    public createProductB(): AbstractProductB {
+        return new ConcreteProductB1();
+    }
+}
+
+
+class ConcreteFactory2 implements AbstractFactory {
+    public createProductA(): AbstractProductA {
+        return new ConcreteProductA2();
+    }
+
+    public createProductB(): AbstractProductB {
+        return new ConcreteProductB2();
+    }
+}
+
+// Since the concrete factory classess are implemented from the Abstract Factory, in which it used the Abstract of Product A and B,
+// so all the subclass can ultilize any of inherited behavior.
+// Factory
+
+// Product A
+interface AbstractProductA {
+    usefulFunctionA(): string;
+}
+
+
+class ConcreteProductA1 implements AbstractProductA {
+    public usefulFunctionA(): string {
+        return 'The result of the product A1.';
+    }
+}
+
+class ConcreteProductA2 implements AbstractProductA {
+    public usefulFunctionA(): string {
+        return 'The result of the product A2.';
+    }
+}
+
+
+// Product A
+
+// Product B
+interface AbstractProductB {
+
+    usefulFunctionB(): string;
+
+    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+}
+
+class ConcreteProductB1 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B1.';
+    }
+
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B1 collaborating with the (${result})`;
+    }
+}
+
+class ConcreteProductB2 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B2.';
+    }
+
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B2 collaborating with the (${result})`;
+    }
+}
+// Product B
+// Either product A or B, when the Abstract Factory used the abstract of Product A / B, any subclass of it can be used by Factory.
+
+
+function clientCode(factory: AbstractFactory) {
+    const productA = factory.createProductA();
+    const productB = factory.createProductB();
+
+    console.log(productB.usefulFunctionB());
+    console.log(productB.anotherUsefulFunctionB(productA));
+}
+
+console.log('Client: Testing client code with the first factory type...');
+clientCode(new ConcreteFactory1());
+
+console.log('');
+
+console.log('Client: Testing the same client code with the second factory type...');
+clientCode(new ConcreteFactory2());
